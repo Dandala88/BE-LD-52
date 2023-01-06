8c16d9bad54e375b54bad812acd1215764874659
Daniel <33705402+Dandala88@users.noreply.github.com>
2023-01-06 16:09:39 -0700

    Initial Commit

---
+++ BE-LD-52/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BE_LD_52.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
